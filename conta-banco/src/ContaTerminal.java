780bc3a33249266191d10e9990760c0abcb567c8
EdimilsonBorges <edimilson-borges159@hotmail.com>
2024-04-27 13:10:53 -0300

    feat: funçoes implementadas

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite seu nome!");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite a Agência!");
+        String agencia = scanner.nextLine();
+        try {
+            System.out.println("Por favor, digite o número da Agência!");
+            int numero = scanner.nextInt();
+
+            System.out.println("Por favor, informe um saldo!");
+            double saldo = scanner.nextDouble();
+
+            scanner.close();
+
+            System.out.println("Olá"
+                    .concat(", ")
+                    .concat(nomeCliente)
+                    .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+                    .concat(agencia)
+                    .concat(", conta " + numero)
+                    .concat(" e seu saldo " + String.format("%.2f",saldo))
+                    .concat(" já está disponível para saque"));
+
+        } catch (InputMismatchException e) {
+            System.out.println("Erro! O valor que você informou está incorreto, por favor informe apenas números válidos!");
+        }
+
     }
 }
